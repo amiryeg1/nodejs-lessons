d492e336e21f5aab11b49933faac487f2e9601c9
amiryeg1 <amir.y2000@yahoo.com>
2020-08-12 13:05:22 +0430

    improvement: JSON web token added

--- L7-mongodb-database/app.js
+++ L7-mongodb-database/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken")
+const config = require("config")
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,10 +19,11 @@ mongoose
   .catch((err) => console.error("could not connect to mongodb...", err));
 
 const userSchema = new mongoose.Schema({
-  Username: { type: String, unique: true },
-  Password: String,
-  FullName: String,
-  Age: Number,
+  Username: { type: String, unique: true, required: true },
+  Password: { type: String, required: true },
+  FullName: { type: String, required: true },
+  Age: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now() }
 });
 
 const User = mongoose.model("User", userSchema);
@@ -49,8 +52,11 @@ app.post("/signup", async (req, res) => {
   });
   const result = await user.save();
   console.log(result);
-
-  res.send("you signed up successfully");
+  let token = jwt.sign({
+    _id:user._id,
+    username:user.Username
+  },config.get("jwtPrivateKey"))
+  res.header('x-auth-token',token).send("you signed up successfully");
 });
 
 app.get("/signup", function (req, res) {
@@ -63,14 +69,19 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
   let unhashedPassword = req.body.password;
-  let person = await User.findOne({ Username: req.body.Username });
-  if (!person) return res.status(400).send("invalid username or password");
+  let user = await User.findOne({ Username: req.body.Username });
+  if (!user) return res.status(400).send("invalid username or password");
 
-  const validPassword = await bcrypt.compare(unhashedPassword, person.Password);
+  const validPassword = await bcrypt.compare(unhashedPassword, user.Password);
   if (!validPassword) {
     res.send("invalid name or password");
   } else {
-    res.send(`${person.FullName} successfully logged in`);
+   let token = jwt.sign({
+      _id:user._id,
+      username:user.Username
+    },config.get("jwtPrivateKey"))
+    console.log(token)
+    res.send(`${user.FullName} successfully logged in`);
   }
 });
 
